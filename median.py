b042b3b63ba94c2ea63bf5118d8bbee3149422ff
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-09-30 23:03:03 +0100

    Added median

--- median.py
+++ median.py
@@ -1,10 +1,30 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def findMedian(numberList):
+    numberList.sort()
+    length = len(numberList)
+    
+    if length%2!=0:
+        ++length
+        length/=2
+        --length
+        return(numberList[int(length)])
+    
+    else:
+        length/=2
+        return((numberList[int(length-1)]+numberList[int(length)])/2)
+        
+        
+    
+    
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(findMedian(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
